eec4f23c1c03c266bc5df4d35f34391bd0c31d53
Jarrett <jbacey@gmail.com>
2016-08-23 16:51:41 -0400

    Account Class - version 2

--- BankAccount/src/ssa/Account.java
+++ BankAccount/src/ssa/Account.java
@@ -1,23 +1,115 @@
 package ssa;
 
-// type of bank accounts
+import java.text.NumberFormat;
+
 public class Account {
 	
-	int accountId;
-	String description;
-	int balance; //amount in the account at any time
+	private int id;
+	private String description;
+	private double balance; //amount in the account at any time
+	static int acctTrack =100;
+	NumberFormat fmt = NumberFormat.getCurrencyInstance();
 	
-	int deposit(int deposit) {
+	double deposit(double deposit) {
 		balance = deposit + balance;
 		return balance;
 	}
 	
-	int withdraw(int withdraw) {
+	double withdraw(double withdraw) {
 		if (withdraw > balance) { 
 		System.out.println("Insufficent funds!");
 		} else		
 		balance = balance - withdraw;
 		return balance;
+	} 
+	
+	// The Transfer Method
+	public void transferFrom(Account account, double amount) {
+		if (account.balance > amount) { 
+		    account.withdraw(amount);
+		    this.deposit(amount);
+			} else	
+				System.out.println("Transfer Fail!");
+	}
+	
+	// Print Method
+	public String print() {
+		return ("Account " + this.id + " balance is " + fmt.format(this.balance));
+	}
+	
+	
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
 	}
 
+	/**
+	 * @param id the id to set
+	 */
+	private void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+
+	/**
+	 * @param balance the balance to set
+	 */
+	private void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	/**
+	 * @param balance
+	 */
+
+	
+	
+	
+	// The class should allow new instances to be created in multiple ways.
+	// #1
+	public Account() {
+		this.balance = 0;
+		this.id = acctTrack;
+		acctTrack += 100;
+	}
+	// #2
+	public Account(int id, String description) {
+		this.balance = 0;
+		this.id = id;
+		this.description = description;
+	}
+	// #3
+	public Account(String description) {
+		this.balance = 0;
+		this.description = description; 
+		this.id = acctTrack;
+		acctTrack += 100;
+	}
 }
+	
+	
+
